8c8a257e890e1efaf06e9e455ae6015bfe9cb90d
kds <kdskoudous98@gmail.com>
2022-12-02 10:38:51 +0100

    innitial commit

---
+++ Backend/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x5539d97b37482d8F6d0720412489dd428E4397e1";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
